86c858c0bbd55e8294eae6e5c58e57c8f10862ae
Fermin <feeralvarez013@gmail.com>
2021-10-29 16:23:25 -0300

    Validacion PIN-Tarjeta

---
+++ sql/banco.sql
@@ -0,0 +1,451 @@
+#Archivo batch (batalllas.sql) para la creaci�n de la 
+#Base de datos del pr�ctico de SQL
+
+# Creamos de la Base de Datos
+CREATE DATABASE banco;
+
+# selecciono la base de datos sobre la cual voy a hacer modificaciones
+USE banco;
+
+# CREACION DE TABLAS ENTIDADES -----------------------------------------------------------------------------------------------------------
+
+CREATE TABLE Ciudad (
+    cod_postal SMALLINT UNSIGNED NOT NULL, 
+    nombre VARCHAR(45) NOT NULL, 
+    
+    CONSTRAINT pk_ciudad 
+    PRIMARY KEY (cod_postal)
+) ENGINE=InnoDB;
+
+CREATE TABLE Sucursal (
+    nro_suc SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    nombre VARCHAR(45) NOT NULL, 
+    direccion VARCHAR(70) NOT NULL, 
+    telefono VARCHAR(45) NOT NULL, 
+    horario VARCHAR(30) NOT NULL, 
+    cod_postal SMALLINT UNSIGNED NOT NULL, 
+    
+    CONSTRAINT pk_sucursal 
+    PRIMARY KEY (nro_suc),
+
+    CONSTRAINT FK_sucur_ubic
+    FOREIGN KEY (cod_postal) REFERENCES Ciudad (cod_postal) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE Empleado (
+    legajo SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    apellido VARCHAR(45) NOT NULL, 
+    nombre VARCHAR(45) NOT NULL, 
+    tipo_doc VARCHAR(20) NOT NULL, 
+    nro_doc INT UNSIGNED NOT NULL, 
+    direccion VARCHAR(70) NOT NULL, 
+    telefono VARCHAR(45) NOT NULL, 
+    cargo VARCHAR(45) NOT NULL, 
+    password VARCHAR(32) NOT NULL,
+    nro_suc SMALLINT UNSIGNED NOT NULL,
+
+    CONSTRAINT pk_empleado 
+    PRIMARY KEY (legajo),
+
+    CONSTRAINT FK_trabaja_en
+    FOREIGN KEY (nro_suc) REFERENCES Sucursal (nro_suc) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE Cliente (
+    nro_cliente MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    apellido VARCHAR(45) NOT NULL, 
+    nombre VARCHAR(45) NOT NULL, 
+    tipo_doc VARCHAR(20) NOT NULL, 
+    nro_doc INT UNSIGNED NOT NULL, 
+    direccion VARCHAR(70) NOT NULL, 
+    telefono VARCHAR(45) NOT NULL,  
+    fecha_nac DATE NOT NULL,
+
+    CONSTRAINT pk_cliente
+    PRIMARY KEY (nro_cliente)
+) ENGINE=InnoDB;
+
+CREATE TABLE Plazo_Fijo (
+    nro_plazo INT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    capital DECIMAL(16,2) UNSIGNED NOT NULL, 
+    tasa_interes DECIMAL(4,2) UNSIGNED NOT NULL, 
+    interes DECIMAL(16,2) UNSIGNED NOT NULL, 
+    fecha_inicio DATE NOT NULL, 
+    fecha_fin DATE NOT NULL, 
+    nro_suc SMALLINT UNSIGNED NOT NULL,
+
+
+    CONSTRAINT pk_plazo_fijo
+    PRIMARY KEY (nro_plazo),
+
+    CONSTRAINT FK_Plazo_Suc
+    FOREIGN KEY (nro_suc) REFERENCES Sucursal (nro_suc) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+#SHOW WARNINGS;
+
+CREATE TABLE Tasa_Plazo_Fijo (
+    periodo SMALLINT UNSIGNED NOT NULL, 
+    monto_inf DECIMAL(16,2) UNSIGNED NOT NULL, 
+    monto_sup DECIMAL(16,2) UNSIGNED NOT NULL, 
+    tasa DECIMAL(4,2) UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_tasa_plazo_fijo
+    PRIMARY KEY (periodo,monto_inf,monto_sup)
+) ENGINE=InnoDB;
+#SHOW WARNINGS;
+
+CREATE TABLE Prestamo (
+    nro_prestamo INT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    fecha DATE NOT NULL, 
+    cant_meses TINYINT UNSIGNED NOT NULL, 
+    monto DECIMAL(10,2) UNSIGNED NOT NULL,  
+    tasa_interes DECIMAL(4,2) UNSIGNED NOT NULL,  
+    interes DECIMAL(9,2) UNSIGNED NOT NULL, 
+    valor_cuota DECIMAL(9,2) UNSIGNED NOT NULL, 
+    nro_cliente MEDIUMINT UNSIGNED NOT NULL, 
+    legajo SMALLINT UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_prestamo
+    PRIMARY KEY (nro_prestamo),
+
+    CONSTRAINT FK_Prestamo_Empleado
+    FOREIGN KEY (legajo) REFERENCES Empleado (legajo) 
+    ON DELETE RESTRICT ON UPDATE CASCADE, 
+
+    CONSTRAINT FK_Prestamo_Cliente
+    FOREIGN KEY (nro_cliente) REFERENCES Cliente (nro_cliente) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+#SHOW WARNINGS;
+
+CREATE TABLE Pago (
+    nro_pago SMALLINT UNSIGNED NOT NULL, 
+    fecha_venc DATE NOT NULL, 
+    fecha_pago DATE , 
+    nro_prestamo INT UNSIGNED NOT NULL,
+
+    CONSTRAINT pk_pago
+    PRIMARY KEY (nro_pago,nro_prestamo),
+
+    CONSTRAINT FK_pago_pr 
+    FOREIGN KEY (nro_prestamo) REFERENCES Prestamo (nro_prestamo) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE Tasa_Prestamo (
+    periodo SMALLINT UNSIGNED NOT NULL, 
+    monto_inf DECIMAL(10,2) UNSIGNED NOT NULL, 
+    monto_sup DECIMAL(10,2) UNSIGNED NOT NULL, 
+    tasa DECIMAL(4,2) UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_tasa_prestamo
+    PRIMARY KEY (periodo,monto_inf,monto_sup)
+) ENGINE=InnoDB;
+#SHOW WARNINGS;
+
+CREATE TABLE Caja_Ahorro (
+    nro_ca INT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    CBU BIGINT UNSIGNED NOT NULL, 
+    saldo DECIMAL(16,2) UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_caja_ahorro
+    PRIMARY KEY (nro_ca)
+) ENGINE=InnoDB;
+#SHOW WARNINGS;
+
+CREATE TABLE Cliente_CA ( 
+    nro_ca INT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    nro_cliente MEDIUMINT UNSIGNED NOT NULL, 
+    
+ 
+    CONSTRAINT pk_clienteCA
+    PRIMARY KEY (nro_cliente,nro_ca),
+
+    CONSTRAINT FK_ClienteCA_CajaAhorro
+    FOREIGN KEY (nro_ca) REFERENCES Caja_Ahorro (nro_ca) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+     
+    CONSTRAINT FK_ClienteCA_Cliente
+    FOREIGN KEY (nro_cliente) REFERENCES Cliente (nro_cliente) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+ 
+) ENGINE=InnoDB;
+
+CREATE TABLE Tarjeta (
+    nro_tarjeta BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    PIN VARCHAR(32)  NOT NULL, 
+    CVT VARCHAR(32)  NOT NULL, 
+    fecha_venc DATE NOT NULL, 
+    nro_cliente MEDIUMINT UNSIGNED NOT NULL, 
+    nro_ca INT UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_tarjeta
+    PRIMARY KEY (nro_tarjeta),
+
+    CONSTRAINT FK_Tarjeta_Cliente_CA
+    FOREIGN KEY (nro_cliente, nro_ca) REFERENCES Cliente_CA (nro_cliente,nro_ca) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE Caja (
+    cod_caja MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+
+    CONSTRAINT pk_caja
+    PRIMARY KEY (cod_caja)
+) ENGINE=InnoDB;
+
+CREATE TABLE Ventanilla (
+    cod_caja MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    nro_suc SMALLINT UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_caja
+    PRIMARY KEY (cod_caja),
+
+    CONSTRAINT FK_es_una_caja_ventanilla
+    FOREIGN KEY (cod_caja) REFERENCES Caja (cod_caja) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+    CONSTRAINT FK_sucur_vent
+    FOREIGN KEY (nro_suc) REFERENCES Sucursal (nro_suc) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE ATM (
+    cod_caja MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    direccion VARCHAR(70) NOT NULL, 
+    cod_postal SMALLINT UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_caja
+    PRIMARY KEY (cod_caja),
+
+    CONSTRAINT FK_ATM_ubic
+    FOREIGN KEY (cod_postal) REFERENCES Ciudad (cod_postal) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+    CONSTRAINT FK_es_una_caja
+    FOREIGN KEY (cod_caja) REFERENCES Caja (cod_caja) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE Transaccion(
+    nro_trans BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    fecha DATE NOT NULL, 
+    hora TIME NOT NULL, 
+    monto DECIMAL(16,2) UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_Transaccion
+    PRIMARY KEY (nro_trans)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE Debito (
+    nro_trans BIGINT UNSIGNED NOT NULL, 
+    descripcion TINYTEXT , 
+    nro_cliente MEDIUMINT UNSIGNED NOT NULL, 
+    nro_ca INT UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_Debito
+    PRIMARY KEY (nro_trans),
+
+    CONSTRAINT FK_Debito_Transaccion
+    FOREIGN KEY (nro_trans) REFERENCES Transaccion (nro_trans) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+
+    CONSTRAINT FK_Debit_Cl
+    FOREIGN KEY (nro_cliente,nro_ca) REFERENCES Cliente_CA(nro_cliente,nro_ca) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE Transaccion_por_caja (
+    nro_trans BIGINT UNSIGNED NOT NULL, 
+    cod_caja MEDIUMINT UNSIGNED NOT NULL,
+
+    CONSTRAINT pk_Transaccion_por_caja
+    PRIMARY KEY (nro_trans),
+
+    CONSTRAINT FK_Transaccion_TransaccionPorCaja
+    FOREIGN KEY (nro_trans) REFERENCES Transaccion (nro_trans) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+    CONSTRAINT FK_Trans_Caja
+    FOREIGN KEY (cod_caja) REFERENCES Caja (cod_caja) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE Deposito (
+    nro_trans BIGINT UNSIGNED NOT NULL, 
+    nro_ca INT UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_Deposito
+    PRIMARY KEY (nro_trans),
+
+    CONSTRAINT FK_Deposito_TransaccionPorCaja
+    FOREIGN KEY (nro_trans) REFERENCES Transaccion_por_caja (nro_trans) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+    CONSTRAINT FK_Depos_CA
+    FOREIGN KEY (nro_ca) REFERENCES Caja_Ahorro (nro_ca) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+
+CREATE TABLE Extraccion (
+    nro_trans BIGINT UNSIGNED NOT NULL, 
+    nro_cliente MEDIUMINT UNSIGNED NOT NULL, 
+    nro_ca INT UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_Extraccion
+    PRIMARY KEY (nro_trans),
+
+    CONSTRAINT FK_Extraccion_TransaccionPorCaja
+    FOREIGN KEY (nro_trans) REFERENCES Transaccion_por_caja (nro_trans) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+    CONSTRAINT FK_Extrac_Cl
+    FOREIGN KEY (nro_cliente,nro_ca) REFERENCES Cliente_CA (nro_cliente,nro_ca) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+
+CREATE TABLE Transferencia (
+    nro_trans BIGINT UNSIGNED NOT NULL, 
+    nro_cliente MEDIUMINT UNSIGNED NOT NULL, 
+    origen INT UNSIGNED NOT NULL, 
+    destino INT UNSIGNED NOT NULL, 
+
+    CONSTRAINT pk_Transferencia
+    PRIMARY KEY (nro_trans),
+
+    CONSTRAINT FK_Transferencia_TransaccionPorCaja
+    FOREIGN KEY (nro_trans) REFERENCES Transaccion_por_caja (nro_trans) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+    CONSTRAINT FK_Extrac_ClienteCA
+    FOREIGN KEY (origen,nro_cliente) REFERENCES Cliente_CA (nro_ca,nro_cliente) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+    CONSTRAINT FK_Extrac_CajaAhorro
+    FOREIGN KEY (destino) REFERENCES Caja_ahorro (nro_ca) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+
+
+CREATE TABLE Plazo_Cliente ( 
+    nro_plazo INT UNSIGNED NOT NULL,
+    nro_cliente MEDIUMINT UNSIGNED NOT NULL, 
+    
+ 
+    CONSTRAINT pk_plazoCliente
+    PRIMARY KEY (nro_plazo,nro_cliente),
+
+    CONSTRAINT FK_PlazoCliente_PlazoFijo
+    FOREIGN KEY (nro_plazo) REFERENCES Plazo_Fijo (nro_plazo) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+     
+    CONSTRAINT FK_PlazoCliente_Cliente
+    FOREIGN KEY (nro_cliente) REFERENCES Cliente (nro_cliente) 
+    ON DELETE RESTRICT ON UPDATE CASCADE
+ 
+) ENGINE=InnoDB;
+
+
+
+
+
+# CREACION DE USUARIOS -----------------------------------------------------------------------------------------------------------
+
+
+# Creamos al usuario ADMIN con contraseña admin que puede conectarse solo desde localhost
+    CREATE USER 'admin'@'localhost'  IDENTIFIED BY 'admin';
+# Otorgamos privilegios correspondientes
+    GRANT ALL PRIVILEGES ON banco.* TO 'admin'@'localhost' WITH GRANT OPTION;
+
+
+#   Eliminamos el usuario vacío
+    #DROP user ''@localhost;
+
+
+# Creamos al usuario EMPLEADO con contraseña empleado
+    CREATE USER 'empleado'@'%' IDENTIFIED BY 'empleado'; 
+# Otorgamos privilegios correspondientes
+    
+    GRANT SELECT ON banco.Empleado TO 'empleado'@'%';
+    GRANT SELECT ON banco.Sucursal TO 'empleado'@'%';
+    GRANT SELECT ON banco.Tasa_Plazo_Fijo TO 'empleado'@'%';
+    GRANT SELECT ON banco.Tasa_Prestamo TO 'empleado'@'%';
+
+    GRANT SELECT, INSERT ON banco.Prestamo TO 'empleado'@'%';
+    GRANT SELECT, INSERT ON banco.Plazo_Fijo TO 'empleado'@'%';
+    GRANT SELECT, INSERT ON banco.Plazo_Cliente TO 'empleado'@'%';
+    GRANT SELECT, INSERT ON banco.Caja_Ahorro TO 'empleado'@'%';
+    GRANT SELECT, INSERT ON banco.Tarjeta TO 'empleado'@'%';
+
+    GRANT SELECT, INSERT, UPDATE ON banco.Cliente_CA TO 'empleado'@'%';
+    GRANT SELECT, INSERT, UPDATE ON banco.Cliente TO 'empleado'@'%';
+    GRANT SELECT, INSERT, UPDATE ON banco.Pago TO 'empleado'@'%';
+
+
+    #Creamos la vista para atm
+    CREATE VIEW trans_cajas_ahorro AS 
+    SELECT * FROM (
+        -- SELECT EXTRACCION
+        (SELECT nro_ca, saldo, nro_trans, fecha, hora, "extraccion" as tipo, monto, cod_caja, nro_cliente, tipo_doc, nro_doc, nombre, apellido, "NULL" AS destino  FROM (Extraccion NATURAL JOIN Transaccion AS SELECTEXTRACCION NATURAL JOIN Transaccion_por_caja)
+
+        NATURAL JOIN
+
+        (SELECT DISTINCT  nro_ca,saldo, nro_cliente, tipo_doc, nro_doc, nombre, apellido FROM ((Caja_ahorro NATURAL JOIN Cliente_CA ) NATURAL JOIN Cliente)) AS SELECTCLIENTE)
+
+        UNION
+
+
+
+        -- SELECT DEBITO
+        (SELECT nro_ca, saldo, nro_trans, fecha, hora, "debito" as tipo, monto, "NULL" as cod_caja, nro_cliente, tipo_doc, nro_doc, nombre, apellido, "NULL" AS destino  FROM (Debito NATURAL JOIN Transaccion AS SELECTDEBITO)
+
+        NATURAL JOIN
+
+        (SELECT DISTINCT  nro_ca,saldo, nro_cliente, tipo_doc, nro_doc, nombre, apellido FROM ((Caja_ahorro NATURAL JOIN Cliente_CA ) NATURAL JOIN Cliente)) AS SELECTCLIENTE)
+
+
+        UNION
+
+        -- SELECT TRANSFERENCIA
+        (SELECT nro_ca, saldo, nro_trans, fecha, hora, "transferencia" as tipo, monto, cod_caja, CLIENTECA.nro_cliente, tipo_doc, nro_doc, nombre, apellido, destino  FROM
+        
+            (SELECT * FROM Transferencia NATURAL JOIN Transaccion NATURAL JOIN Transaccion_por_caja) AS TRANSFERENCIATRANSACCION
+
+
+            JOIN
+
+            (SELECT DISTINCT  nro_ca,saldo, nro_cliente, tipo_doc, nro_doc, nombre, apellido FROM ((Caja_ahorro NATURAL JOIN Cliente_CA) NATURAL JOIN Cliente)) AS CLIENTECA
+            
+            
+            ON
+            CLIENTECA.nro_ca = TRANSFERENCIATRANSACCION.origen AND
+            CLIENTECA.nro_cliente = TRANSFERENCIATRANSACCION.nro_cliente)
+
+       
+
+        UNION
+
+        -- SELECT DEPOSITO
+        (SELECT nro_ca, saldo, nro_trans, fecha, hora, "deposito" as tipo, monto, cod_caja, "NULL" as nro_cliente,"NULL" as tipo_doc,"NULL" as nro_doc,"NULL" as nombre,"NULL" as apellido,"NULL" as destino  FROM (Deposito NATURAL JOIN Transaccion AS SELECTDEPOSITO NATURAL JOIN Transaccion_por_caja)
+
+        NATURAL JOIN
+
+        (SELECT DISTINCT * FROM (Caja_ahorro NATURAL JOIN Cliente_CA )) AS SELECTCLIENTE)) 
+        AS SELECT_VISTA
+
+        ORDER BY nro_ca;
+
+# Creamos al usuario ATM
+    CREATE USER 'atm'@'localhost' IDENTIFIED BY 'atm'; 
+
+#Otorgamos privilegios correspondientes
+    GRANT SELECT, UPDATE ON banco.Tarjeta TO 'atm'@'localhost';
+    GRANT SELECT ON banco.trans_cajas_ahorro TO 'atm'@'localhost';
---
+++ sql/datos.sql
@@ -0,0 +1,145 @@
+# selecciono la base de datos sobre la cual voy a hacer modificaciones
+USE banco;
+
+INSERT INTO Cliente (apellido,nombre,tipo_doc,nro_doc,direccion,telefono,fecha_nac) VALUES ("Alvarez", "Fermin", "DNI", "1111111", "Direccion Fermin","239222222", "2000/10/06");
+INSERT INTO Cliente  (apellido,nombre,tipo_doc,nro_doc,direccion,telefono,fecha_nac) VALUES ("Alvarez", "Hernan", "DNI", "2222222", "Direccion Hernan","2391234", "2002/12/03");
+INSERT INTO Cliente (apellido,nombre,tipo_doc,nro_doc,direccion,telefono,fecha_nac) VALUES ("Alvarez", "Daniel", "DNI", "3333333", "Direccion Daniel","23944332", "2001/11/02");
+INSERT INTO Cliente (apellido,nombre,tipo_doc,nro_doc,direccion,telefono,fecha_nac) VALUES ("Gonzalo", "Selene", "DNI", "4444444", "Direccion Selene","2392234234", "2003/11/08");
+
+
+INSERT INTO Caja_Ahorro(CBU,saldo) VALUES (123,1250);
+INSERT INTO Caja_Ahorro(CBU,saldo) VALUES (785,14000);
+INSERT INTO Caja_Ahorro(CBU,saldo) VALUES (245,25000);
+INSERT INTO Caja_Ahorro(CBU,saldo) VALUES (424,250);
+
+INSERT INTO Cliente_CA VALUES (1,1);
+INSERT INTO Cliente_CA VALUES (2,2);
+INSERT INTO Cliente_CA VALUES (3,3);
+INSERT INTO Cliente_CA VALUES (4,4);
+
+INSERT INTO Caja VALUES ();
+INSERT INTO Caja VALUES ();
+INSERT INTO Caja VALUES ();
+INSERT INTO Caja VALUES ();
+
+-- Para Ventanilla
+INSERT INTO Caja VALUES ();
+INSERT INTO Caja VALUES ();
+INSERT INTO Caja VALUES ();
+
+-- Para ATM
+INSERT INTO Caja VALUES ();
+INSERT INTO Caja VALUES ();
+INSERT INTO Caja VALUES ();
+
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ("2021/9/19", "00:01:20", 450);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ("2021/4/23", "00:11:05", 1200);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ("2021/9/09", "00:21:08", 450);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ("2010/5/19", "01:01:06", 120010);
+INSERT INTO Debito  VALUES (1,"Mi primer debito", 1, 1);
+INSERT INTO Debito  VALUES (2,"Mi segundo debito", 2, 2);
+INSERT INTO Debito  VALUES (3,"Mi tercer debito", 3, 3);
+INSERT INTO Debito  VALUES (4,"Mi cuarto debito", 4, 4);
+
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ("2021/9/19", "00:01:20", 450);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ("2021/4/23", "00:11:05", 1200);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ("2021/9/09", "00:21:08", 450);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ("2010/5/19", "01:01:06", 120010);
+INSERT INTO Transaccion_por_caja VALUES (5, 1);
+INSERT INTO Transaccion_por_caja VALUES (6, 2);
+INSERT INTO Transaccion_por_caja VALUES (7, 3);
+INSERT INTO Transaccion_por_caja VALUES (8, 4);
+INSERT INTO Transferencia VALUES (5, 1, 1, 2);
+INSERT INTO Transferencia VALUES (6, 2, 2, 3);
+INSERT INTO Transferencia VALUES (7, 3, 3, 4);
+INSERT INTO Transferencia VALUES (8, 4, 4, 1);
+
+
+
+
+
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ("2021/9/19", "00:01:20", 450);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ( "2021/4/23", "00:11:05", 1200);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ( "2021/9/09", "00:21:08", 450);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ( "2010/5/19", "01:01:06", 120010);
+INSERT INTO Transaccion_por_caja VALUES (9, 1);
+INSERT INTO Transaccion_por_caja VALUES (10, 2);
+INSERT INTO Transaccion_por_caja VALUES (11, 3);
+INSERT INTO Transaccion_por_caja VALUES (12, 4);
+INSERT INTO Extraccion VALUES (9, 1, 1);
+INSERT INTO Extraccion VALUES (10, 2, 2);
+INSERT INTO Extraccion VALUES (11, 3, 3);
+INSERT INTO Extraccion VALUES (12, 4, 4);
+
+
+
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ( "2021/9/19", "00:01:20", 450);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ( "2021/4/23", "00:11:05", 1200);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ( "2021/9/09", "00:21:08", 450);
+INSERT INTO Transaccion (fecha,hora,monto) VALUES ( "2010/5/19", "01:01:06", 120010);
+INSERT INTO Transaccion_por_caja VALUES (13, 1);
+INSERT INTO Transaccion_por_caja VALUES (14, 2);
+INSERT INTO Transaccion_por_caja VALUES (15, 3);
+INSERT INTO Transaccion_por_caja VALUES (16, 4);
+INSERT INTO Deposito VALUES (13,1);
+INSERT INTO Deposito VALUES (14,2);
+INSERT INTO Deposito VALUES (15,3);
+INSERT INTO Deposito VALUES (16,4);
+
+
+INSERT INTO Ciudad VALUES (6339, "Salliquelo");
+INSERT INTO Ciudad VALUES (8000, "Bahia Blanca");
+INSERT INTO Ciudad VALUES (6500, "Tres lomas");
+
+
+INSERT INTO Sucursal (nombre,direccion,telefono,horario,cod_postal) VALUES  ("Sucursal 1","Alem 11", "239244444", "09hs a 15hs", 6339);
+INSERT INTO Sucursal (nombre,direccion,telefono,horario,cod_postal) VALUES ("Sucursal Bahia","Alem 1001", "291446545", "09hs a 15hs", 8000);
+INSERT INTO Sucursal (nombre,direccion,telefono,horario,cod_postal) VALUES ("Sucursal Tres lomas","Stroeder 450", "2392444242", "09hs a 12hs", 6500);
+
+INSERT INTO Empleado (apellido,nombre,tipo_doc,nro_doc,direccion,telefono,cargo,password,nro_suc) VALUES ("Alvarez","Fermin","DNI",42461930,"Panama 1550", "2392409941","Administrador",md5("Fermin300"),1);
+INSERT INTO Empleado (apellido,nombre,tipo_doc,nro_doc,direccion,telefono,cargo,password,nro_suc) VALUES ("Gonzalo","Selene","DNI",41414141,"Panama 100", "23922521","Ejecutivo",md5("Selene123"),2);
+INSERT INTO Empleado (apellido,nombre,tipo_doc,nro_doc,direccion,telefono,cargo,password,nro_suc) VALUES ("Rogelio","Miranda","DNI",12312333,"Cordoba 100", "232940400","Limpieza",md5("limpiando123"),3);
+
+
+INSERT INTO Plazo_Fijo (capital,tasa_interes,interes,fecha_inicio,fecha_fin,nro_suc) VALUES (1550.50, 3.15, 5.28, "2018/4/12", "2019/9/19", 1);
+INSERT INTO Plazo_Fijo (capital,tasa_interes,interes,fecha_inicio,fecha_fin,nro_suc) VALUES (4550.20, 3.2, 7.28, "2017/5/6", "2017/10/10", 2);
+INSERT INTO Plazo_Fijo (capital,tasa_interes,interes,fecha_inicio,fecha_fin,nro_suc) VALUES (6800.90, 4.15, 8.28, "2018/8/3", "2019/4/4", 3);
+
+
+
+
+INSERT INTO Tasa_Plazo_Fijo VALUES (30, 1000.00, 4500.00, 45.00);
+INSERT INTO Tasa_Plazo_Fijo VALUES (30, 1000.00, 1500.00, 15.00);
+INSERT INTO Tasa_Plazo_Fijo VALUES (30, 8000.00, 3500.00, 10.00);
+
+INSERT INTO Plazo_Cliente VALUES (1, 1);
+INSERT INTO Plazo_Cliente VALUES (2, 2);
+INSERT INTO Plazo_Cliente VALUES (3, 3);
+
+INSERT INTO Prestamo (fecha,cant_meses,monto,tasa_interes,interes,valor_cuota,nro_cliente,legajo) VALUES ("2015/12/12", 4, 12000.50, 4.5, 8, 2000.00, 1, 1);
+INSERT INTO Prestamo (fecha,cant_meses,monto,tasa_interes,interes,valor_cuota,nro_cliente,legajo) VALUES ("2016/04/12", 2, 20000.5, 5.4, 8, 1000.00, 2, 2);
+INSERT INTO Prestamo (fecha,cant_meses,monto,tasa_interes,interes,valor_cuota,nro_cliente,legajo) VALUES ("2017/12/4", 7, 2400.67, 4.5, 8, 240.00, 3, 3);
+
+
+INSERT INTO Pago VALUES (1,"2016/01/02", "2017/01/01", 1);
+INSERT INTO Pago VALUES (2,"2017/01/02", "2017/01/01", 2);
+INSERT INTO Pago VALUES (3,"2018/01/02","2018/01/01", 3);
+
+
+INSERT INTO Tasa_Prestamo VALUES (31,1200.00,1300.00, 4.1 );
+INSERT INTO Tasa_Prestamo VALUES (32,2300.04,2500.00, 5.6 );
+INSERT INTO Tasa_Prestamo VALUES (33,3400.05,3600.00, 6.5 );
+
+
+INSERT INTO Tarjeta (PIN,CVT,fecha_venc,nro_cliente,nro_ca) VALUES (md5("Tarjeta1PIN"),md5("Tarjeta1CVT"),"2018/01/02", 1, 1);
+INSERT INTO Tarjeta (PIN,CVT,fecha_venc,nro_cliente,nro_ca) VALUES (md5("Tarjeta2PIN"),md5("Tarjeta2CVT"),"2019/01/02", 2, 2);
+INSERT INTO Tarjeta (PIN,CVT,fecha_venc,nro_cliente,nro_ca) VALUES (md5("Tarjeta3PIN"),md5("Tarjeta3CVT"),"2014/01/02", 3, 3);
+
+
+INSERT INTO Ventanilla VALUES (5, 1);
+INSERT INTO Ventanilla VALUES (6, 2);
+INSERT INTO Ventanilla VALUES (7, 3);
+
+INSERT INTO ATM VALUES (8, "Direccion ATM 1", 6339);
+INSERT INTO ATM VALUES (9, "Direccion ATM 2", 8000);
+INSERT INTO ATM VALUES (10, "Direccion ATM 3", 6500);
